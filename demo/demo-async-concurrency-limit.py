bb66c5df10b17d81125a3df096aed44b07229cf3
BryanLee <bryanlee@126.com>
2023-07-24 15:31:03 +0800

    feat: add demo-async-concurrency-limit.py

---
+++ demo/demo-async-concurrency-limit.py
@@ -0,0 +1,39 @@
+import asyncio
+from os import urandom
+from random import uniform
+from time import perf_counter
+
+
+async def sleep(sem, cid=None):
+    if cid is None:
+        cid = urandom(5).hex()
+    sec = uniform(0, 2)
+    async with sem:
+        print(f"Coroutine({cid}) will sleep {sec:.2f} seconds")
+        await asyncio.sleep(sec)
+        print(f"Coroutine({cid}) has slept")
+    return sec
+
+
+async def using_gather():
+    coros = init_coroutines()
+    return await asyncio.gather(*coros)
+
+
+async def using_for_loop():
+    coros = init_coroutines()
+    # return [await fut for fut in coros]  # coros will run one by one
+    return [await fut for fut in asyncio.as_completed(coros)]
+
+
+def init_coroutines():
+    sem = asyncio.Semaphore(2)
+    return [sleep(sem) for _ in range(4)]
+
+
+if __name__ == "__main__":
+    st = perf_counter()
+    # result = asyncio.run(using_gather())
+    result = asyncio.run(using_for_loop())
+    et = perf_counter()
+    print(f"total duration: {et - st:.3f}, result: {result}")
