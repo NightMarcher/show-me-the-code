215d8dab6a4d123357387e06995dcac9215a5933
BryanLee <bryanlee@126.com>
2023-07-24 18:37:46 +0800

    feat: limit handle rate

--- demo/demo-async-concurrency-limit.py
+++ demo/demo-async-concurrency-limit.py
@@ -1,17 +1,25 @@
 import asyncio
 from os import urandom
 from random import uniform
-from time import perf_counter
+from time import perf_counter, time
 
 
-async def sleep(sem, cid=None):
+async def handle_sleep(sem):
+    async with sem:
+        res = await sleep()
+
+    print("sleeping")
+    await asyncio.sleep(1)
+    return res
+
+
+async def sleep(cid=None):
     if cid is None:
         cid = urandom(5).hex()
     sec = uniform(0, 2)
-    async with sem:
-        print(f"Coroutine({cid}) will sleep {sec:.2f} seconds")
-        await asyncio.sleep(sec)
-        print(f"Coroutine({cid}) has slept")
+    print(f"Coroutine({cid}) will sleep {sec:.3f} seconds, started at {time():.3f}")
+    await asyncio.sleep(sec)
+    print(f"Coroutine({cid}) has slept")
     return sec
 
 
@@ -28,7 +36,7 @@ async def using_for_loop():
 
 def init_coroutines():
     sem = asyncio.Semaphore(2)
-    return [sleep(sem) for _ in range(4)]
+    return [handle_sleep(sem) for _ in range(4)]
 
 
 if __name__ == "__main__":
